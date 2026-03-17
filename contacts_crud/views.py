103082ccdc9d2075965ae645efd3c0e4931ef065
Aaron Smith <arnhsmith@gmail.com>
2026-03-17 13:08:49 -0400

    added contact list name sorting last name, first name

--- contacts_crud/views.py
+++ contacts_crud/views.py
@@ -6,5 +6,6 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["contacts"] = Contact.objects.all()
+        # Order contacts by last name, then first name for an alphabetical list
+        context["contacts"] = Contact.objects.order_by('last_name', 'first_name')
         return context
